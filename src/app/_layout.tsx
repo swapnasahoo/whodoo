91193a73c92821ab82a3bf503e94f2251f57f523
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-09-12 22:22:15 +0530

    feat: add custom fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,19 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import "../../global.css";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "PlusJakartaSans-Regular": require("../../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "PlusJakartaSans-Medium": require("../../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "PlusJakartaSans-SemiBold": require("../../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "PlusJakartaSans-Bold": require("../../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "PlusJakartaSans-ExtraBold": require("../../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }
