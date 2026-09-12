ac4b2261014a9108c7d1476f6d1b390d3c806577
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-09-12 23:10:48 +0530

    refactor: move CaseCard into a separate component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CaseCard from "../components/CaseCard";
 
 export default function Index() {
   return (
@@ -17,28 +18,7 @@ export default function Index() {
           style={{ flex: 1, marginTop: 24 }}
           showsVerticalScrollIndicator={false}
         >
-          <View className="bg-card px-6 py-2 rounded-md">
-            <View className="flex-row items-center justify-between">
-              <Text className="text-foreground font-jakarta-medium">
-                #001 The Missing Phone
-              </Text>
-
-              <View className="w-20 bg-success/40 border border-success/40 py-0.5 rounded-full">
-                <Text className="text-foreground text-sm font-jakarta-semibold uppercase text-center">
-                  Easy
-                </Text>
-              </View>
-            </View>
-
-            <Text
-              numberOfLines={4}
-              className="text-base text-muted-foreground mt-2"
-            >
-              A beginner friendly challenge designed to test your skills without
-              being too difficult. Perfect for warming up and getting
-              comfortable with the basics.
-            </Text>
-          </View>
+          <CaseCard />
         </ScrollView>
       </SafeAreaView>
     </View>
---
+++ src/components/CaseCard.tsx
@@ -0,0 +1,27 @@
+import { Text, View } from "react-native";
+
+const CaseCard = () => {
+  return (
+    <View className="bg-card px-6 py-2 rounded-md">
+      <View className="flex-row items-center justify-between">
+        <Text className="text-foreground font-jakarta-medium">
+          #001 The Missing Phone
+        </Text>
+
+        <View className="w-20 bg-success/40 border border-success/40 py-0.5 rounded-full">
+          <Text className="text-foreground text-sm font-jakarta-semibold uppercase text-center">
+            Easy
+          </Text>
+        </View>
+      </View>
+
+      <Text numberOfLines={4} className="text-base text-muted-foreground mt-2">
+        A beginner friendly challenge designed to test your skills without being
+        too difficult. Perfect for warming up and getting comfortable with the
+        basics.
+      </Text>
+    </View>
+  );
+};
+
+export default CaseCard;
