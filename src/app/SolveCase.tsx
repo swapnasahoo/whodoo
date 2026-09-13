3256bf574b4f4f3d7d25d8a8cb829a884ad8a6ea
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-09-13 20:19:17 +0530

    feat: enhance SolveCase component with step rendering and answer selection

--- src/app/SolveCase.tsx
+++ src/app/SolveCase.tsx
@@ -1,5 +1,5 @@
 import { mockCases } from "@/data/cases";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { useState } from "react";
 import { Pressable, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -10,6 +10,7 @@ const SolveCase = () => {
 
   const [stepNo, setStepNo] = useState<number>(0);
   const step = caseDetails?.steps[stepNo - 1];
+  const isLastStep: boolean = caseDetails?.steps.length === stepNo;
   const [answerFound, setAnswerFound] = useState<boolean>(false);
 
   if (!caseDetails) return;
@@ -57,8 +58,61 @@ const SolveCase = () => {
           </View>
         </View>
       );
+    } else if (stepNo > 0) {
+      return (
+        <View className="gap-3">
+          <View>
+            <Text className="text-foreground text-lg ml-2 font-jakarta-semibold">
+              {step?.title}
+            </Text>
+
+            <View className="w-1 h-full bg-primary absolute rounded-full" />
+          </View>
+
+          <View>
+            <Text className="text-foreground text-2xl font-jakarta-bold">
+              {step?.question}
+            </Text>
+
+            {step?.options.map((option, index) => {
+              const isCorrectOption =
+                option === step.options[step.correctOption];
+
+              return (
+                <Pressable
+                  key={index}
+                  onPress={() =>
+                    option === step.options[step.correctOption] &&
+                    setAnswerFound(true)
+                  }
+                  disabled={answerFound}
+                  className={`w-full h-20 mt-3 flex-row items-center gap-3 border border-b-6 ${answerFound && isCorrectOption ? "bg-success/15 border-success/20 border-b-success/15" : "bg-card border-b-border/40 border-border"} px-6 rounded-xl transition-all ease-out duration-200 active:scale-[0.98] active:translate-y-1 active:border-0 active:border-b-0`}
+                >
+                  <View
+                    className={`size-10 items-center justify-center rounded-full border ${answerFound && isCorrectOption ? "border-success/20" : "border-border"}`}
+                  >
+                    <Text className="text-foreground font-jakarta-semibold -translate-y-px">
+                      {String.fromCharCode(65 + index)}
+                    </Text>
+                  </View>
+
+                  <Text className="flex-1 text-foreground text-lg font-jakarta-semibold">
+                    {option}
+                  </Text>
+                </Pressable>
+              );
+            })}
+          </View>
+        </View>
+      );
     }
   };
+
+  function handleContinue(): void {
+    setStepNo(stepNo + 1);
+    setAnswerFound(false);
+    isLastStep && router.replace("/");
+  }
 
   return (
     <View className="flex-1 bg-background px-6 py-4">
@@ -89,58 +143,20 @@ const SolveCase = () => {
         {/* CONTENT */}
         <ScrollView style={{ flex: 1, marginTop: 16 }}>
           {renderStep()}
-
-          <View className="gap-3">
-            <View>
-              <Text className="text-foreground text-lg ml-2 font-jakarta-semibold">
-                {step?.title}
-              </Text>
-
-              <View className="w-1 h-full bg-primary absolute rounded-full" />
-            </View>
-
-            <View>
-              <Text className="text-foreground text-2xl font-jakarta-bold">
-                {step?.question}
-              </Text>
-
-              {step?.options.map((option, index) => {
-                const isCorrectOption =
-                  option === step.options[step.correctOption];
-
-                return (
-                  <Pressable
-                    key={index}
-                    onPress={() =>
-                      option === step.options[step.correctOption] &&
-                      setAnswerFound(true)
-                    }
-                    disabled={answerFound}
-                    className={`w-full h-20 mt-3 flex-row items-center gap-3 border border-b-6 ${answerFound && isCorrectOption ? "bg-success/15 border-success/20 border-b-success/15" : "bg-card border-b-border/40 border-border"} px-6 rounded-xl transition-all ease-out duration-200 active:scale-[0.98] active:translate-y-1 active:border-0 active:border-b-0`}
-                  >
-                    <View
-                      className={`size-10 items-center justify-center rounded-full border ${answerFound && isCorrectOption ? "border-success/20" : "border-border"}`}
-                    >
-                      <Text className="text-foreground font-jakarta-semibold -translate-y-px">
-                        {String.fromCharCode(65 + index)}
-                      </Text>
-                    </View>
-
-                    <Text className="text-foreground text-lg font-jakarta-semibold">
-                      {option}
-                    </Text>
-                  </Pressable>
-                );
-              })}
-            </View>
-          </View>
         </ScrollView>
 
-        <Pressable className="bg-violet-700 w-full h-16 items-center justify-center mb-4 rounded-xl border-b-6 border-b-violet-950/40 transition-all duration-200 ease-out active:scale-[0.98] active:border-b-transparent active:translate-y-1">
-          <Text className="text-xl text-foreground font-jakarta-bold">
-            CONTINUE
-          </Text>
-        </Pressable>
+        {answerFound || stepNo === 0 ? (
+          <Pressable
+            onPress={handleContinue}
+            className="bg-violet-700 w-full h-16 items-center justify-center mb-4 rounded-xl border-b-6 border-b-violet-950/40 transition-all duration-200 ease-out active:scale-[0.98] active:border-b-transparent active:translate-y-1"
+          >
+            <Text className="text-xl text-foreground font-jakarta-bold">
+              {isLastStep ? "FINISH" : "CONTINUE"}
+            </Text>
+          </Pressable>
+        ) : (
+          ""
+        )}
       </SafeAreaView>
     </View>
   );
