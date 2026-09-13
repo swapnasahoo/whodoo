d7dbe922948219459f7538fb8710802898f6ab44
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-09-13 17:33:15 +0530

    feat: add SolveCase ui

--- src/app/SolveCase.tsx
+++ src/app/SolveCase.tsx
@@ -1,6 +1,7 @@
 import { mockCases } from "@/data/cases";
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Pressable, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SolveCase = () => {
   const { caseId } = useLocalSearchParams<{ caseId: string }>();
@@ -8,9 +9,81 @@ const SolveCase = () => {
 
   if (!caseDetails) return;
 
+  const difficultyStyles = {
+    Easy: {
+      background: "bg-success/20",
+      border: "border-success/40",
+      text: "text-success",
+    },
+    Medium: {
+      background: "bg-warning/20",
+      border: "border-warning/40",
+      text: "text-warning",
+    },
+    Hard: {
+      background: "bg-destructive/20",
+      border: "border-destructive/40",
+      text: "text-destructive",
+    },
+  };
+
+  const styles = difficultyStyles[caseDetails.difficulty];
+
   return (
-    <View>
-      <Text>Case ID: {caseDetails.id}</Text>
+    <View className="flex-1 bg-background px-6 py-4">
+      <SafeAreaView style={{ flex: 1 }}>
+        {/* HEADER */}
+        <View className="gap-1">
+          <View className="flex-row items-center justify-between">
+            <Text className="text-muted-foreground text-lg font-jakarta-semibold">
+              #{caseDetails.caseNo.toString().padStart(3, "0")}
+            </Text>
+
+            <View
+              className={`w-20 ${styles.background} border ${styles.border} py-0.5 rounded-full`}
+            >
+              <Text
+                className={`${styles.text} text-sm font-jakarta-semibold uppercase tracking-wide text-center`}
+              >
+                {caseDetails.difficulty}
+              </Text>
+            </View>
+          </View>
+
+          <Text className="text-3xl font-jakarta-extrabold text-foreground tracking-wide">
+            {caseDetails.title}
+          </Text>
+        </View>
+
+        {/* CONTENT */}
+        <ScrollView style={{ flex: 1, marginTop: 16 }}>
+          <View className="gap-3">
+            <View>
+              <Text className="text-foreground text-lg font-jakarta-bold tracking-wide ml-2">
+                Case Briefing
+              </Text>
+
+              <View className="w-1 h-full absolute bg-primary rounded-full" />
+            </View>
+
+            <View className="bg-card w-full h-max rounded-md border border-border px-3 py-4 gap-3">
+              <View>
+                <Text className="text-muted-foreground text-[15px] font-jakarta-medium tracking-wide leading-5 ml-4">
+                  {caseDetails.introduction}
+                </Text>
+
+                <View className="w-0.5 h-full absolute bg-primary rounded-full" />
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+
+        <Pressable className="bg-violet-700 w-full h-16 items-center justify-center mb-4 rounded-xl border-b-6 border-b-violet-950/40 transition-all duration-200 ease-out active:scale-[0.98] active:border-b-transparent active:translate-y-1">
+          <Text className="text-xl text-foreground font-jakarta-bold">
+            CONTINUE
+          </Text>
+        </Pressable>
+      </SafeAreaView>
     </View>
   );
 };
